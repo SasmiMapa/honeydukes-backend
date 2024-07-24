d2b655979709e2fbd80d5cfbe9ef1ec949c2b775
SasmiMapa <sasmimapa@gmail.com>
2024-07-24 17:48:16 +0530

    commit

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 
 const router = express.Router();
 
@@ -15,6 +17,9 @@ const upload = multer({
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
   MyRestaurantController.createMyRestaurant
 );
 
